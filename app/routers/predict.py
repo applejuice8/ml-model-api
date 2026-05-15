1af844903376a419b35e6be189fa60520ee35c2f
Colin <colinleonghongseng@gmail.com>
2026-05-15 15:19:20 +0800

    changed the predict hardcoded function

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -2,18 +2,19 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.schemas.predict import PredictRequest, PredictResponse
 from app.dependencies import get_db, validate_api_key
-from app.models import Record, APIKey
+from app.models import APIKey
 from app.services.predict import record_api_usage
 
 
-router = APIRouter(prefix='/predict', tags=['predict'])
+router = APIRouter(tags=['predict'])
 
-@router.post('/v1', response_model=PredictResponse)
+@router.post('/predict', response_model=PredictResponse)
 async def predict(
     req: PredictRequest,
     db_key: APIKey = Depends(validate_api_key),
     db: Session = Depends(get_db)
 ):
     record = record_api_usage(db_key, db)
+    prediction = list(map(lambda x: x**2, req.X_data))
 
-    return PredictResponse(prediction=[1, 2, 3])
+    return PredictResponse(prediction=prediction)
--- test_curl.md
+++ test_curl.md
@@ -16,4 +16,4 @@ TE2bn0hqqMtudQ7TAXQ_Q14_l0iAVdNWFo6DHSYx5qk
 
 # Test Predict
 
-curl -X POST http://127.0.0.1:8000/predict/v1 -H "Content-Type: application/json" -H "X-API-KEY: TE2bn0hqqMtudQ7TAXQ_Q14_l0iAVdNWFo6DHSYx5qk" -d '{"X-data": [1, 2, 3]}'
+curl -X POST http://127.0.0.1:8000/predict -H "Content-Type: application/json" -H "X-API-KEY: TE2bn0hqqMtudQ7TAXQ_Q14_l0iAVdNWFo6DHSYx5qk" -d '{"X-data": [1, 2, 3]}'
